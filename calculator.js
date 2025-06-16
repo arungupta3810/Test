fd93d915b2589ba8f61c4bc403e39430881160dc
arun3810 <125949906+arun3810@users.noreply.github.com>
2025-06-16 22:51:41 +0530

    Support newlines as valid delimiters

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  const parts = numbers.split(",");
+  const normalized = numbers.replace(/\n/g, ",");
+  const parts = normalized.split(",");
   return parts.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-  module.exports = { add };
+  module.exports = { add }; 
